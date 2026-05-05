c6a3f33cf8f34be1bf2f07d74baeb576d17bf649
Danil2537 <dedov7347@gmail.com>
2026-05-05 11:50:22 +0300

    chore: modified entity type enum to try to match list of entity types received from PM

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -21,20 +21,27 @@ export enum JwtTokenType {
 export const PRESIDIO_ANONYMIZER_ANONYMIZE_ENDPOINT = '/anonymize';
 export const PRESIDIO_ANONYMIZER_ANALYZE_ENDPOINT = '/analyze';
 
+// The types of entities we got from out PM don't correspon exactly to Presidio entity types
+// So I made EntityType enum use closest possible types
+// When we add another non-presidio anonymizer later, this list might change
 export enum EntityType {
-  PERSON = 'PERSON',
-  EMAIL_ADDRESS = 'EMAIL_ADDRESS',
+  PERSON = 'PERSON', // Patient's name, surname, patronim etc
   PHONE_NUMBER = 'PHONE_NUMBER',
-  DATE_TIME = 'DATE_TIME',
-  LOCATION = 'LOCATION',
-  URL = 'URL',
-  US_ITIN = 'US_ITIN',
-  US_BANK_NUMBER = 'US_BANK_NUMBER',
-  US_PASSPORT = 'US_PASSPORT',
-  US_DRIVER_LICENSE = 'US_DRIVER_LICENSE',
-  OTHER = 'OTHER',
+  EMAIL_ADDRESS = 'EMAIL_ADDRESS',
+  LOCATION = 'LOCATION', // both residential address and geolocation. Presidio doesn't differentiate them, so we can't either (for now)
+  IP_ADDRESS = 'IP_ADDRESS',
+  MAC_ADDRESS = 'MAC_ADDRESS',
+  MEDICAL_CLINICAL_EVENT = 'MEDICAL_CLINICAL_EVENT', // visits, admissions, hospitalizations, discharges
+  MEDICAL_DISEASE_DISORDER = 'MEDICAL_DISEASE_DISORDER', // diagnosis
+  MEDICAL_MEDICATION = 'MEDICAL_MEDICATION', // perscriptions, drugs
+  MEDICAL_THERAPEUTIC_PROCEDURE = 'MEDICAL_THERAPEUTIC_PROCEDURE', // tests, analysis
+  MEDICAL_BIOLOGICAL_ATTRIBUTE = 'MEDICAL_BIOLOGICAL_ATTRIBUTE', // biometrics
+  MEDICAL_FAMILY_HISTORY = 'MEDICAL_FAMILY_HISTORY', // 'genetics',
+  MEDICAL_HISTORY = 'MEDICAL_HISTORY', // medical records
+  US_SSN = 'US_SSN', // social/insurance
+  UK_NINO = 'UK_NINO', // social/insurance
+  OTHER = 'OTHER', // social/insurance
 }
-
 export enum Confidence {
   LOW = 'low',
   MEDIUM = 'medium',
